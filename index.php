98326749cdf9f90fefabd126bd3fef943fc175b6
Alex <Alex_Neha_92@mail.ru>
2020-04-06 23:22:19 +0300

    Подключен matirealize, сделан шаблон

--- index.php
+++ index.php
@@ -3,8 +3,48 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Heroku test</title>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
+	<nav class="light-blue lighten-1" role="navigation">
+		<div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo">tteacher_nea</a>
+			  	<ul class="right hide-on-med-and-down">
+			    	<li><a href="#"></a></li>
+			  	</ul>
+
+			  	<ul id="nav-mobile" class="sidenav">
+			    	<li><a href="#"></a></li>
+			  	</ul>
+			  <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+		</div>
+	</nav>
+  	<div class="section no-pad-bot" id="index-banner">
+	    <div class="container">
+		    <br><br>
+		    <h1 class="header orange-text">Первая страница</h1>
+		    <div class="row">
+	        	<h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+	        	<p class="header col s12 light">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+	        	<p class="header col s12 light">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+	      	</div>
+	      	<br><br>
+	    </div>
+  	</div>
+
+  	<!--  Scripts-->
+  	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  	<script src="js/init.js"></script>
+  	<!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+	<div class="sidenav-overlay"></div><div class="drag-target"></div></body>
+	
 	
 </body>
 </html>
